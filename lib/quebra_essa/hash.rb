339786cf19f9706500a293a9e46b7e9488e80c0e
Eric Torti <etorti@sp.r7.com>
2014-03-05 17:35:42 -0300

    tentativa frustrada de generalizar o binding de metodos

--- lib/quebra_essa/hash.rb
+++ lib/quebra_essa/hash.rb
@@ -13,14 +13,14 @@ class QuebraEssa::Hash
   def self.bind(obj, method_name)
     def obj.reach(*path)
       QuebraEssa::Hash.reach(self, *path) 
-      # result = nil
-      # hash = self
-      # path.each do |p|
-      #   result = hash[p]
-      #   break if !result.is_a? Hash
-      #   hash = hash[p]
-      # end
-      # result
     end
   end
+
+  # def self.bind(obj, method_name)
+  #   obj.instance_eval do 
+  #     self.class.send(:define_method, method_name) do |*path|
+  #       QuebraEssa::Hash.send(method_name, *path)
+  #     end
+  #   end
+  # end
 end
